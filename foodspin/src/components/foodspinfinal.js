37cf061688ca45c714b471d1a03a4108843aa129
Poonam Gahatraj <poonamgtrz@gmail.com>
2024-06-26 07:27:00 +0545

    foodspin

--- foodspin/src/components/foodspinfinal.js
+++ foodspin/src/components/foodspinfinal.js
@@ -4,8 +4,21 @@ import { useState } from 'react';
 export default function FoodSpinFinal (){
     const [selectedImage, setSelectedImage] = useState(null);
     const [rotationAngle, setRotationAngle] = useState(0);
+
+
+    const foodDetails = {
+        './image1.svg': { name: 'Green Goddess Chicken Salad', price: '$32', description: 'It is a non vegetarian salad which consists of the green goddess dressing mixed with chicken, peppers, olives and celery.' ,color: '#FF6347' },
+        './image2.svg': { name: 'Vegan Avocado Toast', price: '$25', description: 'A delicious vegan toast topped with fresh avocado slices, cherry tomatoes, and arugula.', color: '#FF69B4' },
+        './image3.svg': { name: 'Berry Smoothie Bowl', price: '$18', description: 'A refreshing smoothie bowl made with mixed berries, banana, and almond milk, topped with granola and chia seeds.' , color: '#32CD32'},
+        './image4.svg': { name: 'Classic Caesar Salad', price: '$20', description: 'Crispy romaine lettuce, croutons, and parmesan cheese tossed in Caesar dressing.' , color: '#FFD700' },
+        './image5.svg': { name: 'Grilled Chicken Wrap', price: '$15', description: 'A whole wheat wrap filled with grilled chicken, lettuce, tomatoes, and a tangy sauce.', color: '#FF69B4' },
+        './image1.svg': { name: 'Green Goddess Chicken Salad', price: '$32', description: 'It is a non vegetarian salad which consists of the green goddess dressing mixed with chicken, peppers, olives and celery.', color: '#FFA500' },
+        './image2.svg': { name: 'Vegan Avocado Toast', price: '$25', description: 'A delicious vegan toast topped with fresh avocado slices, cherry tomatoes, and arugula.', color: '#FFD700' },
+        './image3.svg': { name: 'Berry Smoothie Bowl', price: '$18', description: 'A refreshing smoothie bowl made with mixed berries, banana, and almond milk, topped with granola and chia seeds.', color: '#FF69B4' }
+    };
+
     const handleImageClick = (src) => {
-        setSelectedImage(src);
+        setSelectedImage({ ...foodDetails[src], src });
     };
 
     
@@ -21,7 +34,7 @@ export default function FoodSpinFinal (){
         <div style={{border:"1px solid black",height:"100vh",width:"100%",position:"relative",overflow:"hidden"}}>
 
            
-            <div className="Eclipse">
+            <div className="Eclipse" style={{backgroundColor: selectedImage ? selectedImage.color : 'grey' }}>
             <div className='Images'>
 <div className='ImageContainer' style={{transform: `rotate(${rotationAngle}deg)`}}>
 <img src='.\image1.svg' className='Food'  onClick={() => handleImageClick('./image1.svg')}></img>
@@ -51,12 +64,12 @@ export default function FoodSpinFinal (){
 
             {selectedImage && (
                             <div className='SelectedImageContainer'>
-                                <img src={selectedImage} className='SelectedFood' alt='Selected'></img>
+                                <img src={selectedImage.src} className='SelectedFood' alt='Selected'></img>
                             </div>
   )}
   <div  className="Dropdown">
-<button style={{backgroundColor:'#FE912C',border:"none"}} onClick={rotateLeft}><img src='.\dropdown.png'></img></button>
-<button style={{backgroundColor:'#FE912C' ,border:"none"}} onClick={rotateRight}><img src='.\dropdown.png'></img></button>
+<button style={{backgroundColor: selectedImage ? selectedImage.color : 'grey' ,border:"none"}} onClick={rotateLeft}><img src='.\dropdown.png'></img></button>
+<button style={{backgroundColor: selectedImage ? selectedImage.color : 'grey' ,border:"none"}} onClick={rotateRight}><img src='.\dropdown.png'></img></button>
   </div>
 
             <div className="Header">
@@ -73,12 +86,14 @@ export default function FoodSpinFinal (){
 
         </div>
 
-        <div style={{marginTop:"15%"}}>
-                <h1>$32</h1>
-                <h1 style={{width:"20%"}}>Green Goddess Chicken Salad</h1>
-                <p style={{width:"25%"}}>It is a non vegetarian salad which consists of the green goddess dressing mixed with chicken, peppers, olives and celery. </p>
-                <button style={{padding:"10px",borderRadius:"20px",backgroundColor:"#FE912C",border:"none"}}>ORDER NOW</button>
-            </div>
+        {selectedImage && (
+                    <div className="Details" style={{ marginTop: "15%" }}>
+                        <h1 style={{ color: selectedImage.color }}>{selectedImage.price}</h1>
+                        <h1 style={{ width: "20%" }}>{selectedImage.name}</h1>
+                        <p style={{ width: "25%" }}>{selectedImage.description}</p>
+                        <button style={{ padding: "10px", borderRadius: "20px", backgroundColor: selectedImage.color , border: "none" }}>ORDER NOW</button>
+                    </div>
+                )}
         </div>
         </>
     )
